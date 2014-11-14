6589ef21f104452559a69b710eb26bd89545d38a
Jose Contreras <jocontre@joselcontreras.com>
2014-11-14 12:20:03 -0600

    Initial Swift files

---
+++ Swiftcard/Swiftcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftcard
+//
+//  Created by Jose Contreras on 11/14/14.
+//  Copyright (c) 2014 JLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
